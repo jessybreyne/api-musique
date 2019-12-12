4687a5bd611970a93d28a06f70a7c258325cab5a
Axel SERRIE <axel.serrie@gmail.com>
2019-12-12 10:49:40 +0100

    Ajout du CRUD de chaque élément

---
+++ src/Form/ArtisteType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artiste;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtisteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('photo')
+            ->add('musiques')
+            ->add('albums')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Artiste::class,
+        ]);
+    }
+}
