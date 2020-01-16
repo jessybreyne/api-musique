e3873df3d62f1fa3f2b0914556bc9e0a75ff5959
Alexandre Guibreteau <aguibreteau@info26-07.iut45.univ-orleans.fr>
2020-01-16 11:34:40 +0100

    Modif affichage Admin

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -13,10 +15,22 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
+            ->add('roles', TextType::class)
             ->add('password')
             ->add('APIKey')
             ->add('playlists')
+        ;
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($tagsAsArray) {
+                    // transform the array to a string
+                    return implode(', ', $tagsAsArray);
+                },
+                function ($tagsAsString) {
+                    // transform the string back to an array
+                    return explode(', ', $tagsAsString);
+                }
+            ))
         ;
     }
 
